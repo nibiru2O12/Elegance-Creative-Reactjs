e0a2c5fe2cf108e87f2044ce4242a618b4b9fedc
rj <robert.gaguitr@gmail.com>
2019-03-25 17:38:23 +0800

    added style on home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
-import homeImg from '../images/item-1.jpg';
+import homeImg from "../images/item-1.jpg";
 
 const Home = () => {
   return (
-      <Container className="section">
-        <p>HELLO, WELCOME TO</p>
+    <div className="section">
+      <SectionContent>
+        <p className="greetings">HELLO, WELCOME TO</p>
         <p className="title">ELEGANCE</p>
         <p className="desc">
           This is a clean and modern HTML5 template with a video background. You
           can use this layout for your profile page. Please spread a word about
           templatemo to your friends. Thank you.
         </p>
-      </Container>
+      </SectionContent>
+    </div>
   );
 };
 
-const Container = styled.div`
+const SectionContent = styled.div`
+  
+  color: white;
+  text-align: center; 
+  width: 45vw;
+  margin-left: auto;
+  margin-right: auto;
+
+  p { 
+    padding: 0px;
+    margin: 0px;
+  }
+
+  & .greetings {
+    font-size: 28px;
+  }
+
+  & .title {
+    font-size: 94px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  & .desc {
+    font-size: 20px;
+    line-height: 1.5;
+  }
+
 `;
 
 export default Home;
